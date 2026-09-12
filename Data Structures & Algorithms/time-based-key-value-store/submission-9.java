8e1ca53bcbcde1a4c1f65004fad838a8ff61fdb6
Khush Khandelwal <61112799+khushkhandelwal05@users.noreply.github.com>
2026-09-12 14:42:17 +0530

    Add: time-based-key-value-store - submission-9

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-9.java
@@ -0,0 +1,44 @@
+class TimeMap {
+
+    HashMap<String, ArrayList<Pair>> map;
+
+    class Pair {
+        int time;
+        String val;
+
+        public Pair(int time, String val) {
+            this.time = time;
+            this.val = val;
+        }
+    }
+
+    public TimeMap() {
+        map = new HashMap<>(); 
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        ArrayList<Pair> pa = map.getOrDefault(key, new ArrayList<>());
+        pa.add(new Pair(timestamp, value));
+        // Collections.sort(pa, (a, b) -> a.time - b.time);
+        map.put(key, pa);
+    }
+    
+    public String get(String key, int timestamp) {
+        ArrayList<Pair> pa = map.getOrDefault(key, new ArrayList<>());
+        if (pa.size() == 0) return "";
+        int s = 0;
+        int e = pa.size() - 1;
+        int ans = -1;
+        while(s <= e) {
+            int mid = s + ((e - s) / 2);
+            if(pa.get(mid).time == timestamp) return pa.get(mid).val;
+            if(pa.get(mid).time > timestamp) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+                ans = mid;
+            }
+        }
+        return ans == -1 ? "" : pa.get(ans).val;
+    }
+}
